8942bb3e6ab052aff52e242d87a86e97a9a0a9a2
Rae <raefeldman@gmail.com>
2020-03-15 18:21:14 -0700

    setup again

---
+++ models/book.js
@@ -0,0 +1,15 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Book extends Sequelize.Model {}
+  Book.init({
+    title: Sequelize.STRING,
+    author: Sequelize.STRING,
+    year: Sequelize.INTEGER
+  }, {
+    sequelize
+  });
+
+  return Book;
+};
